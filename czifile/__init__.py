6e109266e40bdb8c96bc964abc076235c0b6b63e
Dave Williams <cdave@uw.edu>
2018-03-26 16:41:30 -0700

    Update links to README.md

--- czifile/__init__.py
+++ czifile/__init__.py
@@ -1,6 +1,6 @@
 from .czifile import imread, CziFile, Segment, xml_reader, match_filename
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __all__ = (
     'imread', 'CziFile', 'Segment', 'xml_reader', 'match_filename'
 )
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import re
 import os
 
 
-with open('README.rst') as f:
+with open('README.md') as f:
     readme = f.read()
 
 with open('czifile/__init__.py') as f:
